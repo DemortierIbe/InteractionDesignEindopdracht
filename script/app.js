bbf5a367d78f9de6e12156ea536df8da46611873
DemortierIbe <ibe.demortier@student.howest.be>
2022-11-20 22:17:18 +0100

    hondjes op website

--- script/app.js
+++ script/app.js
@@ -10,14 +10,29 @@ let getAPI = async (lat, lon) => {
 	// Eerst bouwen we onze url op
 	const link = `https://api.TheDogAPI.com/v1/breeds`;
 	// Met de fetch API proberen we de data op te halen.
-	const city = await getData(link);
+	const dogs = await getData(link);
+	showFoto(dogs);
 	// Als dat gelukt is, gaan we naar onze showResult functie.
-	console.log(city);
-	
+	console.log(dogs);
   };
+
+let showFoto = function(dogs) {
+	let doghtml = '';
+	for(let dog of dogs)
+  {
+	// console.log(dog.image.url);
+    doghtml += `<div class="c-rectangle__kader">
+	<div class="c-rectangle">
+	<img src="${dog.image.url}">
+	</div>
+	</div>`
+  }
+  document.querySelector('.js-dogs').innerHTML = doghtml;
+};
 
 
 document.addEventListener('DOMContentLoaded', function () {
 	getAPI();
+	
 });
 
