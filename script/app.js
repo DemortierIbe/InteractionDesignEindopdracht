a63ed6490b99bce008e106ae037bbb93b0695d3d
DemortierIbe <ibe.demortier@student.howest.be>
2022-11-27 13:34:15 +0100

    foto's zelfde grote

--- script/app.js
+++ script/app.js
@@ -1,38 +1,30 @@
 const getData = (endpoint) => {
-	return fetch(endpoint)
-	  .then((r) => r.json())
-	  .catch((e) => console.error(e));
-  };
-
-
+  return fetch(endpoint)
+    .then((r) => r.json())
+    .catch((e) => console.error(e));
+};
 
 let getAPI = async (lat, lon) => {
-	// Eerst bouwen we onze url op
-	const link = `https://api.TheDogAPI.com/v1/breeds`;
-	// Met de fetch API proberen we de data op te halen.
-	const dogs = await getData(link);
-	showFoto(dogs);
-	// Als dat gelukt is, gaan we naar onze showResult functie.
-	console.log(dogs);
-  };
+  // Eerst bouwen we onze url op
+  const link = `https://api.TheDogAPI.com/v1/breeds`;
+  // Met de fetch API proberen we de data op te halen.
+  const dogs = await getData(link);
+  showFoto(dogs);
+  // Als dat gelukt is, gaan we naar onze showResult functie.
+  console.log(dogs);
+};
 
-let showFoto = function(dogs) {
-	let doghtml = '';
-	for(let dog of dogs)
-  {
-	// console.log(dog.image.url);
-    doghtml += `<div class="c-rectangle__kader">
-	<div class="c-rectangle">
+let showFoto = function (dogs) {
+  let doghtml = '';
+  for (let dog of dogs) {
+    // console.log(dog.image.url);
+    doghtml += `<div class="c-rectangle">
 	<img src="${dog.image.url}">
-	</div>
-	</div>`
+	</div>`;
   }
   document.querySelector('.js-dogs').innerHTML = doghtml;
 };
 
-
 document.addEventListener('DOMContentLoaded', function () {
-	getAPI();
-	
+  getAPI();
 });
-
