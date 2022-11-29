a37b41edffe80e5fb8af91b0a65d33908a2081a2
DemortierIbe <ibe.demortier@student.howest.be>
2022-11-30 00:03:55 +0100

    werkt niet

--- script/app.js
+++ script/app.js
@@ -9,7 +9,7 @@ let getAPI = async (lat, lon) => {
   const link = `https://api.TheDogAPI.com/v1/breeds`;
   // Met de fetch API proberen we de data op te halen.
   const dogs = await getData(link);
-  showFoto(dogs);
+  // showFoto(dogs);
   // Als dat gelukt is, gaan we naar onze showResult functie.
   console.log(dogs);
 };
@@ -19,12 +19,27 @@ let showFoto = function (dogs) {
   for (let dog of dogs) {
     // console.log(dog.image.url);
     doghtml += `<div class="c-rectangle">
-	<img src="${dog.image.url}">
+	<img  src="${dog.image.url}">
   <p class="c-name_dogs">${dog.name}</p>
+  <p class="c-name_dogs">${dog.weight.imperial}</p>
+  <p class="c-name_dogs">${dog.temperament}</p>
+  <p class="c-name_dogs">${dog.description}</p>
 	</div>`;
   }
-  document.querySelector('.js-dogs').innerHTML = doghtml;
+  document.querySelector('.js-dogs').innerHTML += doghtml;
 };
+
+const listentodog = function() {
+  let hond = document.querySelectorAll('.js-dogs')
+    hond.forEach(Element => {
+      hond.addEventListener('click', function(){
+
+      })
+    });
+  
+};
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
   getAPI();
